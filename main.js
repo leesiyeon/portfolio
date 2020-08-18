642a4c20de84cbcd7c30af3374813d9a1f3e6e38
leesiyeon <kaaaaaate33@gmail.com>
2020-08-18 15:42:36 +0900

    JS: navbar

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// navbar를 올리고 내릴때 투명하게
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
+  } else {
+    navbar.classList.remove("navbar--dark");
+  }
+});
